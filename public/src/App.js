e6246c66feb827d5c19ebc90aa015db021605a84
yashwanth6 <yashu654321@gmail.com>
2022-03-25 11:40:37 -0700

    First commit

---
+++ public/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+import Header from "./components/layout/Header"
+import Content from "./components/Content"
+import Footer from "./components/layout/Footer"
+import Login from "./components/login/Login"
+
+import "./css/style.css"
+
+const App = () => {
+
+  const [appData, setAppData] = useState({
+    showContent: false,
+    showBackButton: false,
+  })
+
+
+  const toggleBackButton = (showBackButton) => {
+    setAppData({ ...appData, showBackButton })
+  }
+
+  const onSuccessLogin = (userInfo) => {
+    setAppData({ ...appData, userInfo, showContent: true })
+  }
+
+  const { showContent, userInfo, showBackButton } = appData
+  return (
+    <div className="container">
+      {/* including the Title and other components */}
+      <Header userInfo={userInfo} />
+      {showContent == false ? (
+        <Login onSuccessLogin={onSuccessLogin} />
+      ) : (
+        <Content
+          userInfo={userInfo}
+          showBackButton={showBackButton}
+          toggleBackButton={toggleBackButton}
+        />
+      )}
+      <Footer
+        showBackButton={showBackButton}
+        toggleBackButton={toggleBackButton}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ public/src/components/layout/Title.js
@@ -0,0 +1,8 @@
+// Title component
+export default () => {
+  return (
+    <div className="title">
+       <span className="bold-title">Chate</span>
+    </div>
+  )
+}
